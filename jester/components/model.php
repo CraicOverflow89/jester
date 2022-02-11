a836b1e31727e99328ee9c6abcbf74a123c3eeae
CraicOverflow89 <craic.overflow@hotmail.com>
2022-02-11 23:19:19 +0000

    Updated database connection. Added table and entity methods to model.

--- jester/components/model.php
+++ jester/components/model.php
@@ -2,12 +2,78 @@
 
 namespace Jester\Components;
 
+use \Jester\Libraries\Database;
+
 abstract class Model {
 
-	abstract public function create();
+	protected static $tableName = '';
+	protected static $fieldList = [];
+	protected $entityID = null;
+	protected $entityData = [];
 
-	public function getByID($id): Model {
-		// TODO: lookup $table using $id
+	public function __construct(int $id, array $data) {
+		$this->entityID = $id;
+		$this->entityData = $data;
+	}
+
+	final public function __toString(): string {
+		return 'Model<' . static::$tableName . ', id ' . $this->entityID . ', ' . json_encode($this->entityData) . '>';
+	}
+
+	final public static function create(array $data): int {
+		return Database::insert('INSERT INTO ' . static::$tableName . ' ('
+			. implode(', ', array_keys(static::$fieldList))
+			. ') VALUES ('
+			. implode(', ', array_map(fn($it) => ':' . $it, array_keys(static::$fieldList)))
+			. ')',
+		$data);
+	}
+
+	final public static function createTable(): void {
+		Database::invoke('CREATE TABLE ' . static::$tableName . '('
+			. 'id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, '
+			. implode(', ', array_map(fn($it) => $it . ' ' . static::$fieldList[$it], array_keys(static::$fieldList)))
+			. ')'
+		);
+	}
+
+	final public static function getByID(int $id): Model|null {
+		$data = Database::select('SELECT id, '
+			. implode(', ', array_keys(static::$fieldList))
+			. ' FROM ' . static::$tableName
+			. ' WHERE id = :id',
+		[
+			'id' => $id,
+		]);
+		if(!count($data)) {
+			return null;
+		}
+		$result = [];
+		foreach(static::$fieldList as $field => $type) {
+			$result[$field] = $data[0][$field];
+		}
+		return static::init($id, $result);
+	}
+
+	abstract protected static function init(int $id, array $data): Model;
+
+	final public function toArray(): array {
+		return array_merge($this->entityData, [
+			'id' => $this->entityID,
+		]);
+	}
+
+	final public function toJSON(): string {
+		return json_encode($this->toArray());
+	}
+
+	final public function save(): void {
+		Database::update('UPDATE ' . static::$tableName . ' SET '
+			. implode(', ', array_map(fn($it) => ':' . $it, array_keys(static::$fieldList)))
+			. ' WHERE id = :id',
+		array_merge($this->entityData, [
+			'id' => $this->entityID,
+		]));
 	}
 
 }
--- jester/libraries/database.php
+++ jester/libraries/database.php
@@ -7,41 +7,45 @@ class Database {
 	private static $connection = null;
 	private static $errorMessage = null;
 
-	public static function connect($host, $database, $username, $password) {
-		self::$connection = (function() use ($host, $database, $username, $password) {
+	public static function connect(string $host, string $port, string $database, string $username, string $password): void {
+		self::$connection = (function() use ($host, $port, $database, $username, $password) {
 			try {
-				$db = new \PDO('mysql:host=$host;dbname=$database', $username, $password);
-				$db -> setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+				$db = new \PDO('mysql:host=' . $host . ';port=' . $port . ';dbname=' . $database, $username, $password);
+				$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 				return $db;
 			}
 			catch(\PDOException $ex) {
-				self::$errorMessage = $ex -> getMessage();
-				return null;
+				self::$errorMessage = $ex->getMessage();
+				throw $ex;
 			}
 		})();
 	}
 
-	private static function execute($sql, $parameterMap = []) {
-		$query = self::$connection -> prepare($sql);
-		$query -> execute($parameterMap);
+	private static function execute(string $sql, array $parameterMap = []) {
+		$query = self::$connection->prepare($sql);
+		$query->execute($parameterMap);
 		return $query;
 	}
 
-	public static function getErrorMessage() {
+	public static function getErrorMessage(): string {
 		return self::$errorMessage;
 	}
 
-	public static function insert($sql, $parameterMap = []) {
+	public static function insert(string $sql, array $parameterMap = []): int {
 		self::execute($sql, $parameterMap);
-		return self::$connection -> lastInsertId();
+		return self::$connection->lastInsertId();
 	}
 
-	public static function select($sql, $parameterMap = []) {
-		return self::execute($sql, $parameterMap) -> fetchAll();
+	public static function invoke(string $sql, array $parameterMap = []): void {
+		self::execute($sql, $parameterMap);
 	}
 
-	public static function update($sql, $parameterMap = []) {
-		self::execute($sql, $parameterMap) -> rowCount();
+	public static function select(string $sql, array $parameterMap = []) {
+		return self::execute($sql, $parameterMap)->fetchAll();
+	}
+
+	public static function update(string $sql, array $parameterMap = []): int {
+		return self::execute($sql, $parameterMap)->rowCount();
 	}
 
 }
